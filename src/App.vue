4f851e8846a11dcb3d33015dff615950df512958
marlonchiu <Knight1898@outlook.com>
2023-04-21 22:48:34 +0800

    导航菜单

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,75 @@
 <template>
+  <Header></Header>
   <router-view></router-view>
 </template>
 
+<script setup>
+import Header from '@/components/Header.vue'
+import { useRoute, useRouter } from 'vue-router'
+// 获取路由信息
+let route = useRoute()
+// 执行路由的跳转
+let router = useRouter()
+</script>
+
 <style>
+.ant-menu-submenu {
+  z-index: 1000000000 !important;
+}
+
+body .ant-menu-sub .ant-menu-item {
+  height: auto;
+}
+.ant-menu-sub .ant-menu-item .prod-item {
+  display: flex;
+  width: 400px;
+  justify-content: space-between;
+}
+.ant-menu .prod-item img {
+  width: 100px;
+  height: 100px;
+}
+.ant-menu-item .prod-item .middle {
+  width: 220px;
+  height: 100px;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+}
+
+.ant-menu-item .prod-item .middle .title {
+  font-size: 16px;
+  font-weight: 900;
+}
+.ant-menu-item .prod-item .middle .content {
+  display: flex;
+  justify-content: space-between;
+}
+.ant-menu-item .prod-item .middle .btn {
+  display: inline-block;
+  width: 30px;
+  height: 30px;
+  text-align: center;
+  line-height: 30px;
+  border: 1px solid #ccc;
+  margin: 0 5px;
+}
+.ant-menu-item .prod-item .right .price {
+  font-weight: 900;
+  color: orange;
+  font-size: 16px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+.ant-menu-item .total {
+  font-weight: 900;
+  display: flex;
+  justify-content: flex-end;
+  font-size: 16px;
+}
+.ant-menu-item .total .num {
+  color: orange;
+  padding-right: 20px;
+}
 </style>
---
+++ src/components/Header.vue
@@ -0,0 +1,106 @@
+<template>
+  <div class="header">
+    <div class="logo" @click="router.push('/')">
+      <img src="@/assets/img/logo_rect.jpg" alt="logo" />
+    </div>
+    <a-input-search v-model:value="data.value" placeholder="搜索商品或商品号" class="input-search" @search="onSearch" />
+    <a-menu v-model:selectedKeys="data.current" mode="horizontal">
+      <a-menu-item key="help">
+        <template #icon>
+          <question-circle-outlined />
+        </template>
+        帮助
+      </a-menu-item>
+      <a-menu-item key="salesroom">
+        <template #icon>
+          <ShopOutlined />
+        </template>
+        门店
+      </a-menu-item>
+      <a-sub-menu key="account">
+        <template #icon>
+          <UserOutlined />
+        </template>
+        <template #title>账户</template>
+      </a-sub-menu>
+      <a-sub-menu key="buycart">
+        <template #icon>
+          <CarOutlined />
+        </template>
+        <template #title>购物车</template>
+        <a-menu-item-group title="购物商品">
+          <a-menu-item v-for="(item, i) in store.state.buycarts" :key="item.id">
+            <div class="prod-item">
+              <div class="left">
+                <img :src="item.imgsrc" :alt="item.title" />
+              </div>
+              <div class="middle">
+                <div class="title">{{ item.title }}</div>
+                <div class="content">
+                  <span class="num">数量：{{ item.num }}</span>
+                  <div class="control">
+                    <span class="btn" @click.stop="store.commit('addBuycartsNum', i)">+</span>
+                    <span class="btn" @click.stop="store.commit('minusBuycartsNum', i)">-</span>
+                  </div>
+                </div>
+              </div>
+              <div class="right">
+                <div class="price">¥ {{ item.price * item.num }}</div>
+              </div>
+            </div>
+          </a-menu-item>
+          <a-menu-item key="totalPrice">
+            <div class="total">
+              <span>总价：</span>
+              <span class="num"> ¥ {{ store.getters.totalPrice }}</span>
+            </div>
+          </a-menu-item>
+        </a-menu-item-group>
+      </a-sub-menu>
+    </a-menu>
+  </div>
+</template>
+
+<script setup>
+import { useRoute, useRouter } from 'vue-router'
+import { useStore } from 'vuex'
+import { reactive } from 'vue'
+import { ShopOutlined, UserOutlined, CarOutlined, QuestionCircleOutlined } from '@ant-design/icons-vue'
+const store = useStore()
+const router = useRouter()
+const data = reactive({
+  value: '',
+  current: ['help']
+})
+const onSearch = () => {}
+</script>
+
+<style lang="less" scoped>
+.header {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  position: fixed;
+  z-index: 1000000;
+  width: 100%;
+  background-color: #fff;
+  box-shadow: 0 0 10px #ccc;
+  transition: all 0.5s;
+}
+.header.hidden {
+  transform: translate(0, -100%);
+}
+
+.logo {
+  margin-right: 40px;
+  img {
+    height: 30px;
+  }
+}
+.input-search {
+  display: flex;
+  flex: 1;
+  height: 40px;
+  align-items: center;
+}
+</style>
