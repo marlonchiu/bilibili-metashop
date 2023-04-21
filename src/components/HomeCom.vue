c8f6812b6d3f85ea03e3268c37be928d29db4b93
marlonchiu <Knight1898@outlook.com>
2023-04-21 22:36:14 +0800

    Suspense异步组件使用实现Loading加载效果

---
+++ src/components/HomeCom.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>异步首页</div>
+  <ul>
+    <li v-for="(item, index) in hero" :key="index">{{ item.name }}</li>
+  </ul>
+</template>
+
+<script setup>
+import { reactive } from 'vue'
+import { getHomepage } from '@/api/index'
+
+const result = await getHomepage()
+const hero = reactive(result.hero)
+console.log('result: ', result)
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,15 +1,35 @@
+<!--
+ * @Description:
+ * @Author: marlonchiu
+ * @Date: 2023-03-10 12:05:04
+ * @LastEditors: marlonchiu
+ * @LastEditTime: 2023-04-21 22:28:55
+-->
 <template>
-  <div class="home">
-    <h1>首页</h1>
-  </div>
+  <suspense>
+    <template #fallback>
+      <div class="loading">
+        <Loading></Loading>
+      </div>
+    </template>
+    <template #default>
+      <h1>
+        <HomeAsync></HomeAsync>
+      </h1>
+    </template>
+  </suspense>
 </template>
 
 <script setup>
-import { onMounted } from 'vue'
+import { onMounted, defineAsyncComponent } from 'vue'
 import { getHomepage } from '@/api/index'
+import Loading from '../components/Loading.vue'
+const HomeAsync = defineAsyncComponent(() => import('../components/HomeCom.vue'))
 
 onMounted(async () => {
-  const result = await getHomepage()
-  console.log('result: ', result);
+  // const result = await getHomepage()
+  // console.log(result)
 })
 </script>
+
+<style lang="less" scoped></style>
